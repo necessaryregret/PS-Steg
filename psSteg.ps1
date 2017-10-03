ab1a67b9632bb8f7163e977bb4bc6b51424e3a51
JeffersonOrwell <chris.hill.man@gmail.com>
2017-10-03 12:21:58 -0400

    Update psSteg.ps1
    Yep, math... arrays and math, not my friends.  I think I have it now.  Need better base64 string terminator.

--- psSteg.ps1
+++ psSteg.ps1
@@ -4,14 +4,16 @@ $sectext = "Secret Text is in here! and here and here!";
 $bytes = [System.IO.File]::readallbytes($filename);
 $encodedsectext = [convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($sectext));
 [array]$base64characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".ToCharArray();
- for($i=0; $i -lt $bytes.length-1; $i++){
+ for($i=0; $i -lt $bytes.length; $i++){
         if(($bytes[$i]-eq 255)-and($bytes[$i+1] -eq 218)){
-            $sos = $i +2;
+            $sos = $i +1;
             }
          }
- for($i=0; $i -lt $encodedsectext.length-1; $i++){
+ for($i=0; $i -lt $encodedsectext.length; $i++){
             [char]$enctext = $encodedsectext[$i];
             $bytes[$i+$sos+$offset] = $base64characters.IndexOf($enctext);
          }
+
 if ($enctext -ne "="){ echo " not = "; $bytes[$i+1+$sos+$offset] = 64;}
+#wow is stripping last character... why??
 [system.io.file]::WriteAllBytes("bearout.jpg", $bytes);
